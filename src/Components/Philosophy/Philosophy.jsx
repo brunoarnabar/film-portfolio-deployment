83e4a47140231cef90e0eacce637fe350bf61c99
bra40 <bruno.arnabar@yahoo.com>
2022-06-18 18:17:40 -0400

    phil_color_blocks

--- src/Components/Philosophy/Philosophy.jsx
+++ src/Components/Philosophy/Philosophy.jsx
@@ -6,8 +6,44 @@ function Philosophy() {
     
     <div className="philContainer">
         <div className="Backdrop">
-            <div className="First Message">
-                Philosophy
+            <div className="HeadingPh">
+                My Philosophy
+            </div>
+
+            <div className="SVG One">
+
+            </div>
+
+            <div className="Message First">
+                I Like It
+            </div>
+
+            <div className="SVG Two">
+
+            </div>
+
+            <div className="Message Second">
+                I Like It So Much
+            </div>
+
+            <div className="SVG Three">
+
+            </div>
+
+            <div className="Message Third">
+                They Taught Me How To See The World
+            </div>
+
+            <div className="SVG Four">
+
+            </div>
+
+            <div className="Message Final">
+                And I Wont Look Back On That
+            </div>
+
+            <div className="SVG Five">
+
             </div>
         </div>
     </div>
