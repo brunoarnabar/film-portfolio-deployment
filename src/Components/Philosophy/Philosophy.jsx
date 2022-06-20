d65253d4830992832247171bb77dbc98d928c14a
bra40 <bruno.arnabar@yahoo.com>
2022-06-20 15:19:37 -0400

    pilosophy.2

--- src/Components/Philosophy/Philosophy.jsx
+++ src/Components/Philosophy/Philosophy.jsx
@@ -6,45 +6,20 @@ function Philosophy() {
     
     <div className="philContainer">
         <div className="Backdrop">
-            <div className="HeadingPh">
-                My Philosophy
-            </div>
+            <div className="HeadingPh"> My Philosophy </div>
+            <div className="SVG One"> </div>
 
-            <div className="SVG One">
+            <div className="Message First"> I’ve always loved how films reflect our cognitive processes and allows us to create profound connections from the formal elements we percieve on screen. </div>
+            <div className="SVG Two"> </div>
 
-            </div>
+            <div className="Message Second"> Hearing "Creative Director & Developer" may sound like an oxymoron (at least, to me it did). However, I realized my skillset has been defined by that very role. </div>
+            <div className="SVG Three"> </div>
 
-            <div className="Message First">
-                I Like It
-            </div>
+            <div className="Message Third"> Blending film and computer science, we can create beautiful and meaningful UI that improves our collective experiences through narrative. </div>
+            <div className="SVG Four"> </div>
 
-            <div className="SVG Two">
-
-            </div>
-
-            <div className="Message Second">
-                I Like It So Much
-            </div>
-
-            <div className="SVG Three">
-
-            </div>
-
-            <div className="Message Third">
-                They Taught Me How To See The World
-            </div>
-
-            <div className="SVG Four">
-
-            </div>
-
-            <div className="Message Final">
-                And I Wont Look Back On That
-            </div>
-
-            <div className="SVG Five">
-
-            </div>
+            <div className="Message Final"> I'm passionate about using my knowledge of art and technology to create innovative solutions and working with a team to explore modern approaches for supporting artistic expression. </div>
+            <div className="SVG Five"> </div>
         </div>
     </div>
 
