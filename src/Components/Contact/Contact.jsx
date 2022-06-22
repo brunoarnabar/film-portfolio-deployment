4b6bae4c7aea32d3e8c32a41d0aecb74b735ba5a
bra40 <bruno.arnabar@yahoo.com>
2022-06-22 13:50:42 -0400

    Contact.Start

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -9,7 +9,7 @@ import HeyImg from "../../Images/ListIcons/sayHey.jpg"
 import Modal from 'react-modal'
 import { Form, Input, TextArea, Button } from 'semantic-ui-react';
 import emailjs from 'emailjs-com';
-import Swal from 'sweetalert2';
+// import Swal from 'sweetalert2';
 
 const SERVICE_ID = "gmail";
 const TEMPLATE_ID = "porfolio_template";
@@ -28,17 +28,17 @@ const handleOnSubmit = (e) => {
     emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, USER_ID)
       .then((result) => {
         console.log(result.text);
-        Swal.fire({
-          icon: 'success',
-          title: 'Message Sent Successfully'
-        })
+        // Swal.fire({
+        //   icon: 'success',
+        //   title: 'Message Sent Successfully'
+        // })
       }, (error) => {
         console.log(error.text);
-        Swal.fire({
-          icon: 'error',
-          title: 'Ooops, something went wrong',
-          text: error.text,
-        })
+        // Swal.fire({
+        //   icon: 'error',
+        //   title: 'Ooops, something went wrong',
+        //   text: error.text,
+        // })
       });
     e.target.reset()
   };
@@ -69,11 +69,21 @@ function Contact() {
 <div className='List ThreeDir'>
     <button onClick={clickMe} className='Option'>
         <div className='OptImg'> <img src={HeyImg} alt='HeyImg' className='ListImg'/> </div>
-        <div className='OptPromt'> I just want to say hello. </div>
+        <div className='OptPromt HelloOpt'> I just want to say hello. </div>
     </button>
 </div>
 
-<Modal isOpen={dirModalIsOpen} onRequestClose={() => setDirModalIsOpen(false)}>
+<Modal isOpen={dirModalIsOpen} onRequestClose={() => setDirModalIsOpen(false)}
+onAfterOpen={() => {
+  document.body.style.top = `-${window.scrollY}px`
+  document.body.style.position = 'fixed'
+}}
+onAfterClose={() => {
+  const scrollY = document.body.style.top
+  document.body.style.position = ''
+  document.body.style.top = ''
+  window.scrollTo(0, parseInt(scrollY || '0') * -1)
+}}>
     <button onClick={() => setDirModalIsOpen(false)}>x</button>
     <Form onSubmit={handleOnSubmit}>
         <Form.Field
