3b539d381cadd148df1da11ecbaaea264eec3fa3
bra40 <bruno.arnabar@yahoo.com>
2022-07-01 02:22:52 -0400

    Contact.Messy {style.2}

--- src/Components/Contact/FormContainer/FormCon.jsx
+++ src/Components/Contact/FormContainer/FormCon.jsx
@@ -171,8 +171,8 @@ function FormCont({closeModal}) {
                   type='text'
                   required
               >
-                <option value="default">How'd You Find Me</option>
-                <option value="We've Previously Worked Together">We've Previously Worked Together</option>
+                <option value="We've Chatted In Person">We've Chatted In Person</option>
+                <option value="By Working Together Previously">By Working Together Previously</option>
                 <option value="Through A Recommendation">Through A Recommendation</option>
                 <option value="Discovered On The Web">Discovered On The Web</option>
                 <option value="Found On Social Media">Found On Social Media</option>
