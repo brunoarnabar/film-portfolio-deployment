236df1239c8e8790447aac1478c6180ac0f2a61e
bra40 <bruno.arnabar@yahoo.com>
2022-07-02 17:36:02 -0400

    Contact.Messy {style.4}

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -8,10 +8,72 @@ import FormCon from "./FormContainer/FormCon";
 Modal.setAppElement('#root')
 
 function Contact() {
+
+  const [one, setOne] = useState(false);
+  const [two, setTwo] = useState(false);
+  const [three, setThree] = useState(false);
+
+
+
     const [dirModalIsOpen, setDirModalIsOpen] = useState(false)
     
     const openModal = () => { setDirModalIsOpen(true); document.body.style.overflow = 'hidden'; }
-    const closeModal = () => { setDirModalIsOpen(false); document.body.style.overflow = 'unset'; }
+
+    const openModalOne = () => { 
+      setDirModalIsOpen(true); 
+      document.body.style.overflow = 'hidden'; 
+      setOne(current => !current);}
+
+      const openModalTwo = () => { 
+        setDirModalIsOpen(true); 
+        document.body.style.overflow = 'hidden'; 
+        setTwo(current => !current);}
+
+        const openModalThree = () => { 
+          setDirModalIsOpen(true); 
+          document.body.style.overflow = 'hidden'; 
+          setThree(current => !current);}
+
+    const closeModal = () => { 
+      setDirModalIsOpen(false); 
+      document.body.style.overflow = 'unset';
+      setOne(false);
+      setTwo(false);
+      setThree(false);}
+
+     function outputHTML(one, two, three) {
+        if(!one & !two & !three) return null;
+        else if(one){
+          return (
+            <div className='List'>
+              <div className='Option formHead'>
+              <div className='OptImg'> <img src={DirImg} alt='DirImg' className='ListImg'/> </div>
+              <div className='OptPromt'> I want to work with you as a director. </div>
+              </div>
+            </div>
+         )
+        }
+        else if(two){
+          return (
+            <div className='List'>
+              <div className='Option formHead'>
+                <div className='OptImg'> <img src={DevImg} alt='DevImg' className='ListImg'/> </div>
+                <div className='OptPromt'> I want to work with you as a developer. </div>
+              </div>
+            </div>
+         )
+        }
+        else{
+          return (
+            <div className='List'>
+              <div className='Option formHead hey'>
+                  <div className='OptImg'> <img src={HeyImg} alt='HeyImg' className='ListImg'/> </div>
+                  <div className='OptPromt formHead'> I just want to say hello. </div>
+              </div>
+            </div>
+         )
+        }
+     }
 
   return (
     <div className="contactContainer">
@@ -21,21 +83,21 @@ function Contact() {
             <br/>
 
       <div className='List OneDir'>
-          <button onClick={openModal} className='Option' >
+          <button onClick={openModalOne} className='Option' >
               <div className='OptImg'> <img src={DirImg} alt='DirImg' className='ListImg'/> </div>
               <div className='OptPromt'> I want to work with you as a director. </div>
           </button>
       </div>
 
       <div className='List TwoDir'>
-          <button onClick={openModal} className='Option'>
+          <button onClick={openModalTwo} className='Option'>
               <div className='OptImg'> <img src={DevImg} alt='DevImg' className='ListImg'/> </div>
               <div className='OptPromt'> I want to work with you as a developer. </div>
           </button>
       </div>
 
       <div className='List ThreeDir'>
-          <button onClick={openModal} className='Option'>
+          <button onClick={openModalThree} className='Option'>
               <div className='OptImg'> <img src={HeyImg} alt='HeyImg' className='ListImg'/> </div>
               <div className='OptPromt HelloOpt'> I just want to say hello. </div>
           </button>
@@ -52,7 +114,9 @@ onAfterClose={() => {
   window.scrollTo(0, parseInt(scrollY || '0') * -1)
 }} >
             <div className='formHeader'>
-              HEADER
+              <div>
+                {outputHTML(one, two, three)}
+              </div>
             </div>
             <div className='form-container-contact'>
               <FormCon className='formStyle' closeModal={closeModal} />
