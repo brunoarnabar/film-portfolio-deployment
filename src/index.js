07eee4a45cf3269955b690e8488d3af12ce6438a
bra40 <bruno.arnabar@yahoo.com>
2022-08-26 11:09:06 -0400

    WentBackToOldCommit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import ReactDom from "react-dom/client";
+// import ReactDom from "react-dom/client";
+import { render } from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Greenless from "./Components/Sample/Films/Greenless";
 import GoBack from "./Components/Sample/Helpers/GoBack";
 
-const root = ReactDom.createRoot(document.getElementById("root"));
-root.render(
+const root = document.getElementById('root');
+// const root = ReactDom.createRoot(document.getElementById("root"));
+// root.render(
+  render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
@@ -20,5 +23,6 @@ root.render(
         }
       />
     </Routes>
-  </BrowserRouter>
+  </BrowserRouter>,
+  root
 );
