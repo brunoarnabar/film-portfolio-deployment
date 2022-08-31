7a0b661d267542a32a84926a2da7d6275faa7067
bra40 <bruno.arnabar@yahoo.com>
2022-08-30 20:47:55 -0400

    CleaningRedundancy

--- src/Components/Skill/MobileRoulette/MobileRoulette.jsx
+++ src/Components/Skill/MobileRoulette/MobileRoulette.jsx
@@ -1,59 +1,52 @@
-import { React, useEffect, useCallback } from "react";
+import { React, useEffect, useState } from "react";
 import { Carousel } from "react-responsive-carousel";
 import Steps from "../Roulette/Steps";
 import "./mobileRoulette.scss";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 
 export default function MobileRoulette(props) {
+  const [currentSlide, setCurrentSlide] = useState(0);
 
-  // function updateCurrentSlide(index) {
-  //     if (props.slideIndex !== index) {
-  //     props.setSlideIndex(index);
-  //   }
-  // }
-
-  const updateCurrentSlide = useCallback(
-    () => {
-      if (props.slideIndex !== index) {
-        props.setSlideIndex(index);
-      }
-    },
-    [index, props.slideIndex, props.setSlideIndex]
-  );
+  function updateCurrentSlide(index) {
+    if (currentSlide !== index) {
+      setCurrentSlide(index);
+      props.setSlideIndex(index);
+    }
+  }
 
   useEffect(() => {
-    updateCurrentSlide(props.slideIndex);
-  }, [props.slideIndex, updateCurrentSlide]);
+    setCurrentSlide(props.slideIndex);
+  }, [props.slideIndex]);
 
   return (
-    <Carousel
-      className="CarouselSlide"
-      showThumbs={false}
-      infiniteLoop={true}
-      autoPlay={true}
-      showStatus={false}
-      showArrows={false}
-      swipeable={true}
-      emulateTouch={true}
-      interval={9500}
-      useKeyboardArrows={false}
-      selectedItem={props.slideIndex}
-      onChange={updateCurrentSlide}
-      stopOnHover={false}
-      swipeScrollTolerance={25}
-      preventMovementUntilSwipeScrollTolerance={true}
-    >
-      {props.info.map((info) => (
-        <div className="blue" key={info.id}>
-          <Steps
-            steps={info.steps}
-            ex1={info.ex1}
-            ex2={info.ex2}
-            ex3={info.ex3}
-            key={info.id}
-          />
-        </div>
-      ))}
-    </Carousel>
+      <Carousel
+        className="CarouselSlide"
+        showThumbs={false}
+        infiniteLoop={true}
+        autoPlay={true}
+        showStatus={false}
+        showArrows={false}
+        swipeable={true}
+        emulateTouch={true}
+        interval={9500}
+        useKeyboardArrows={false}
+        selectedItem={currentSlide}
+        onChange={updateCurrentSlide}
+        stopOnHover={false}
+        swipeScrollTolerance={25}
+        preventMovementUntilSwipeScrollTolerance={true}
+      >
+        {props.info.map((info) => (
+          <div className="blue" key={info.id}>
+            <Steps
+              steps={info.steps}
+              ex1={info.ex1}
+              ex2={info.ex2}
+              ex3={info.ex3}
+              key={info.id}
+            />
+          </div>
+        ))}
+      </Carousel>
   );
 }
