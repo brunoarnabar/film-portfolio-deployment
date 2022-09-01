691a673f26cc5c3180da00932f90bc8f628b72dd
bra40 <bruno.arnabar@yahoo.com>
2022-09-01 17:56:15 -0400

    FrustratedWithNavigation

--- src/Components/Skill/MobileRoulette/MobileRoulette.jsx
+++ src/Components/Skill/MobileRoulette/MobileRoulette.jsx
@@ -1,8 +1,8 @@
 import { React, useEffect, useState } from "react";
 import { Carousel } from "react-responsive-carousel";
 import Steps from "../Roulette/Steps";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "./mobileRoulette.scss";
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 
 export default function MobileRoulette(props) {
   const [currentSlide, setCurrentSlide] = useState(0);
