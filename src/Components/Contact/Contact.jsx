642f04638016e0041b976dd1b14e243e050fa13d
bra40 <bruno.arnabar@yahoo.com>
2022-09-14 10:56:16 -0400

    Working Modal

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from "react";
+import React, { useState} from "react";
 import "./contact.scss";
 import Modal from "react-modal";
 import FormCont from "./FormContainer/FormCont";
