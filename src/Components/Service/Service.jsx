bfafb3cb52c4dd587f508bb8c811e0e94b2942ce
bra40 <bruno.arnabar@yahoo.com>
2022-09-16 02:07:13 -0400

    SCSS Cleaning

--- src/Components/Service/Service.jsx
+++ src/Components/Service/Service.jsx
@@ -4,30 +4,20 @@ import ServiceEntry from "./ServiceEntry";
 import { IoDesktopOutline } from "react-icons/io5";
 import { GiFilmStrip } from "react-icons/gi";
 import { FaPeopleCarry } from "react-icons/fa";
-// import {FaHandsHelping, FaPeopleCarry} from "react-icons/fa";
 
 const entryOne = {
   sub: "Programming",
-  txt1: "I’m a programmer with experience in both the front and back end of production. I’m driven to work on technologies surrounding film and art, and I’m excited to work with you on your next project.",
-  
-  // txt2: "I understand the importance of a well-rounded and complete product, and I’ll be enthusiastic to collaborate with you to help achieve your vision. I’ll work with you to create a product that is not only functional, but also beautiful, user-friendly, and efficient.",
-  // txt3: "I’ll work with you to understand your vision for the project, and I’ll use my skills and knowledge to help you bring that vision to life. I’ll also be available to answer any questions and to provide support throughout the project."
+  txt: "I’m a programmer with experience in both the front and back end of production. I’m driven to work on technologies surrounding film and art, and I’m excited to work with you on your next project.",
 };
 
 const entryTwo = {
   sub: "Filmmaking",
-  txt1: "I can write and create a compelling narrative to be produced independently or commercially. I can direct actors and bring a vision from a page to the screen.",
-  
-  // txt2: "I understand the importance of a well-rounded and complete product, and I’ll be enthusiastic to collaborate with you to help achieve your vision. I’ll work with you to create a product that is not only functional, but also beautiful, user-friendly, and efficient.",
-  // txt3: "I’ll work with you to understand your vision for the project, and I’ll use my skills and knowledge to help you bring that vision to life. I’ll also be available to answer any questions and to provide support throughout the project."
+  txt: "I can write and create a compelling narrative to be produced independently or commercially. I can direct actors and bring a vision from a page to the screen.",
 };
 
 const entryThree = {
   sub: "Combination",
-  txt1: "My education has been multi-disiplinary, so there's lots of factors that cross-over between each field. I'm detail-oriented and have a knack for creativity. I'd love to bring those skills to your team.",
-  
-  // txt2: "I understand the importance of a well-rounded and complete product, and I’ll be enthusiastic to collaborate with you to help achieve your vision. I’ll work with you to create a product that is not only functional, but also beautiful, user-friendly, and efficient.",
-  // txt3: "I’ll work with you to understand your vision for the project, and I’ll use my skills and knowledge to help you bring that vision to life. I’ll also be available to answer any questions and to provide support throughout the project."
+  txt: "My education has been multi-disiplinary, so there's lots of factors that cross-over between each field. I'm detail-oriented and have a knack for creativity. I'd love to bring those skills to your team.",
 };
 
 export default function Service() {
--- src/Components/Service/ServiceEntry.jsx
+++ src/Components/Service/ServiceEntry.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "./service.scss";
 
-export default function ServiceEntry({ icon, sub, txt1, txt2, txt3 }) {
+export default function ServiceEntry({ icon, sub, txt}) {
   return (
     <div className="ServiceEntry box-shadow-dark text-neutral-400">
       {icon}
       <div className="ServiceCardText">
         <div className="title text-neutral-200">{sub}</div>
-        <div className="textBody text-neutral-200">{txt1}</div>
+        <div className="textBody text-neutral-200">{txt}</div>
       </div>
     </div>
   );
