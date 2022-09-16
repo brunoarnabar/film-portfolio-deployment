fab6947b690a25bd7748841dd3074caf3b7ec576
bra40 <bruno.arnabar@yahoo.com>
2022-09-16 14:03:00 -0400

    Birds Eye View

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Greenless from "./Components/Sample/Films/FilmComponent/Greenless";
 import TheBox from "./Components/Sample/Films/FilmComponent/TheBox";
+import BirdsEyeView from "./Components/Sample/Films/FilmComponent/BirdsEyeView";
 // import Nav from "./Components/Nav/Nav"
 
 //goToContext
@@ -13,32 +14,42 @@ import { GoToContactContextProvider } from "./Contexts/GoToContactContext";
 import { AboutClickerProvider } from "./Contexts/AboutClickerContext";
 import { ContactClickerProvider } from "./Contexts/ContactClickerContext";
 
+
 const root = ReactDom.createRoot(document.getElementById("root"));
 root.render(
-    <AboutClickerProvider>
-      <ContactClickerProvider>
-        <GoToContactContextProvider>
-          <GoToAboutContextProvider>
-            <BackToSampleProvider>
-              <BrowserRouter>
-                {/* <Nav/> */}
-                <Routes>
-                  <Route path="/" element={<App />} />
-                  <Route path="/Greenless" element={<Greenless />} />
-                  <Route path="/TheBox" element={<TheBox />} />
-                  <Route
-                    path="*"
-                    element={
-                      <div style={{ backgroundColor: "var(--clr-primary-900)", height: "100vh" }}>
-                        <p>There's nothing here :/ New sections are coming though! :) Check out what I've currently got at brunoarnabar.com</p>
-                      </div>
-                    }
-                  />
-                </Routes>
-              </BrowserRouter>
-            </BackToSampleProvider>
-          </GoToAboutContextProvider>
-        </GoToContactContextProvider>
-      </ContactClickerProvider>
-    </AboutClickerProvider>
+  <AboutClickerProvider>
+    <ContactClickerProvider>
+      <GoToContactContextProvider>
+        <GoToAboutContextProvider>
+          <BackToSampleProvider>
+            <BrowserRouter>
+              {/* <Nav/> */}
+              <Routes>
+                <Route path="/" element={<App />} />
+                <Route path="/Greenless" element={<Greenless />} />
+                <Route path="/TheBox" element={<TheBox />} />
+                <Route path="/BirdsEyeView" element={<BirdsEyeView />} />
+                <Route
+                  path="*"
+                  element={
+                    <div
+                      style={{
+                        backgroundColor: "var(--clr-primary-900)",
+                        height: "100vh",
+                      }}
+                    >
+                      <p>
+                        There's nothing here :/ New sections are coming though!
+                        :) Check out what I've currently got at brunoarnabar.com
+                      </p>
+                    </div>
+                  }
+                />
+              </Routes>
+            </BrowserRouter>
+          </BackToSampleProvider>
+        </GoToAboutContextProvider>
+      </GoToContactContextProvider>
+    </ContactClickerProvider>
+  </AboutClickerProvider>
 );
